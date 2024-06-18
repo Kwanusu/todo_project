dc5ba3cca50293c131f879381c6bd6790bdc1ac9
Joseph <kwanusujoseph@gmail.com>
2024-06-18 17:39:34 +0300

    initialized a superuser, added a navbar, created template inheritance and added css

--- todo_app/views.py
+++ todo_app/views.py
@@ -6,27 +6,27 @@ from .forms import TaskForm
 def tasks_list(request):
     tasks = Task.objects.all()
     return render(request, 'task_list.html', {'tasks': tasks})
-
+# Retrieves all task objects from the database and renders tasklist.html
 def add_task(request):
     if request.method == 'POST':
         form = TaskForm(request.POST)
         if form.is_valid():
             form.save()
             return redirect('task_list')
-        
+# Checks if method is poat and creates a tskform instance with submitted data        
     else:
-        form = TaskForm()
+        form = TaskForm()# If method is not post creates an empty taskform instance
     return render(request, 'add_task.html', {'form': form}) 
 
-def update_task(request, pk):
+def update_task(request, pk):#Retrieves task with given primary key, if not found raises 404 error
     task = get_object_or_404(Task, pk=pk)       
     if request.method == 'POST':
         form = TaskForm(request.POST, instance=task)
         if form.is_valid():
             form.save()
-            
+ # Checks request method and creates ataskform instance with the submitted data, validates form and saves it if valid           
     else:
-        form = TaskForm(instance=task)
+        form = TaskForm(instance=task)# Creates a taskform instance if method is not post
     return render(request, 'update_task.html', {'form': form})
 
 def delete_task(request, pk):
--- todo_project/settings.py
+++ todo_project/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-$lg$ft2u179dkkgnv0x+_up*1evf2)c*s0nzmu9#_=dxpy^hrs'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
